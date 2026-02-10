98155aba60e297e030b9efac5c2a1e213dd8ee90
Nyle-Creator <aba.clintnyle25@gmail.com>
2026-02-10 11:15:10 +0800

    Add files via upload

---
+++ ABA - Lab act 1.py
@@ -0,0 +1,20 @@
+Name = "Clint Nyle S. Aba"
+age = 20
+Heigth = 1.6764
+Currently_Working = False
+
+print("Fullname:",Name)
+print(type(Name))
+print()
+
+print("Age:",age)
+print(type(age))
+print()
+
+print("Heigth_in_meters:", Heigth)
+print(type(Heigth))
+print()
+
+print("Currently_Working?:", Currently_Working)
+print()
+print(type(Currently_Working))
