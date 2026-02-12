00377350b0bed8dd4c240cbdd38266afc9a3aaa8
Nyle-Creator <aba.clintnyle25@gmail.com>
2026-02-12 22:14:44 +0800

    Add files via upload

---
+++ ABA - LAB ACT 2.py
@@ -0,0 +1,10 @@
+name = ""
+age = 0
+
+print("----------------------------")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print()
+
+print(f"Kon'nichiwa, {name}!, who is already {age} years old")
+print("-----------------------------------------------")
