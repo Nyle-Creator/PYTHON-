8ab611f91231f96c2c2380ef6b8d8c23007b7bc8
Nyle-Creator <aba.clintnyle25@gmail.com>
2026-03-07 22:57:19 +0800

    Add files via upload

---
+++ ABA- WEEK 3.py
@@ -0,0 +1,7 @@
+print("----------------------------")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print()
+
+print(f"Kon'nichiwa, {name}!, who is already {age} years old")
+print("-----------------------------------------------")
